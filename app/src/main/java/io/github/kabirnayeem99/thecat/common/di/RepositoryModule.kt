5f6593344aa9b8e6203d0eb61e359a3cc706dafe
Aaron M. Wilson <aaron@hire-programmers.com>
2022-01-21 18:27:46 +0600

    🐛 fix: fix build issue

--- app/src/main/java/io/github/kabirnayeem99/thecat/common/di/RepositoryModule.kt
+++ app/src/main/java/io/github/kabirnayeem99/thecat/common/di/RepositoryModule.kt
@@ -4,9 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import io.github.kabirnayeem99.thecat.data.apiService.CatApiService
 import io.github.kabirnayeem99.thecat.data.dataSource.CatRemoteDataSource
-import io.github.kabirnayeem99.thecat.data.repository.CatPagingSource
+import io.github.kabirnayeem99.thecat.data.repository.CatPagingSourceRepository
 import io.github.kabirnayeem99.thecat.data.repository.CatRepositoryImpl
 import io.github.kabirnayeem99.thecat.domain.repository.CatRepository
 
@@ -15,11 +14,11 @@ import io.github.kabirnayeem99.thecat.domain.repository.CatRepository
 class RepositoryModule {
     @Provides
     fun provideCatRepository(dataSource: CatRemoteDataSource): CatRepository {
-        return CatRepositoryImpl(dataSource)
+        return CatRepositoryImpl()
     }
 
     @Provides
-    fun provideCatPagingSource(apiService: CatApiService): CatPagingSource {
-        return CatPagingSource(apiService)
+    fun provideCatPagingSource(dataSource: CatRemoteDataSource): CatPagingSourceRepository {
+        return CatPagingSourceRepository(dataSource)
     }
 }
